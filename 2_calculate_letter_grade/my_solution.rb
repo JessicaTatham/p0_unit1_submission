e4a9d85107595678ade86e48bdb689c46ff3f9e2
Jessica Tatham <jectatham@gmail.com>
2014-03-25 21:03:32 -0700

    Update my_solution.rb

--- 2_calculate_letter_grade/my_solution.rb
+++ 2_calculate_letter_grade/my_solution.rb
@@ -63,3 +63,21 @@ average = sum/array.length
 # We had to fix the syntax of the argument to lowercase the 'array'.
 
 # 4. Reflection 
+
+	#What parts of your strategy worked? What problems did you face?
+# Dave and I just went step by step through the pseudocode and testing. We had previously done part of this challenge on
+# Socrates, so we were able to go back and review what we did there and adapt it to fit the code. 
+	#What questions did you have while coding? What resources did you find to help you answer them?
+# This was a pretty straight forward challenge. Once we figured out how to use rspec in the last challenge, we ran it in this
+# challenge with no problem.
+	#What concepts are you having trouble with, or did you just figure something out? If so, what?
+# I understand this challenge. I suppose if there is one thing I don't understand to it's fullest is the .inject method, but as
+# I work with it more, I will come to understand it.
+	#Did you learn any new skills or tricks?
+# No tricks :).
+	#How confident are you with each of the learning objectives?
+# I feel confident in the learning objectives.
+	#Which parts of the challenge did you enjoy?
+# I enjoyed pairing with this challenge.
+	#Which parts of the challenge did you find tedious?
+# Nothing tedious!
